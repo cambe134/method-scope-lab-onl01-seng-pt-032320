b2258bd21d700f0b0598ba107984e9a8f111f119
Spencer Cambern <cambern134@gmail.com>
2020-04-30 08:59:59 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,5 +1,12 @@
-require "pry"
 def rescue_princess_peach
   status = "rescued"
   puts "Hooray! Mario has rescued Princess Peach."
 end
+
+
+
+
+## Comment the below back in to see method scope in action!
+
+# rescue_princess_peach
+# puts status
